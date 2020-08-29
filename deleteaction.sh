1e68c47daac000f610f0aaa01d6a60756d72b4cb
HomeToken <stephan.thierry@live.com>
2020-08-29 23:10:10 +0200

    Support for delete directories

--- deleteaction.sh
+++ deleteaction.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 
 files=$(echo $INPUT_REMOTEFILES | tr ";" "\n")
+dirs=$(echo $INPUT_REMOTEDIRECTORIES | tr ";" "\n")
 
 echo "" > rmcmd
 if [ "$INPUT_IGNORESSL" -eq "1" ]
@@ -14,6 +15,12 @@ for file in $files
 do
     echo -e "mrm \"$file\";\n" >> rmcmd
 done
+
+for dir in $dirs
+do
+    echo -e "rm -r \"$dir\";\n" >> rmcmd
+done
+
 echo -e "quit;\n" >> rmcmd
 
 lftp  ftp://$INPUT_HOST < rmcmd
